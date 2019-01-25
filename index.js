a52fca4d45a9b3b551399622c1ae7bc13117e76d
Andrew Bituin <fandrewbituin@gmail.com>
2019-01-25 14:26:16 -0800

    added completed url string using API key added to end

--- index.js
+++ index.js
@@ -2,30 +2,47 @@
 /* global $ */
 
 const apiKey = '50tWslfPEMTASf8Nddf6CgMuUIOAhvpt0WswKZIp';
+const baseUrl = 'https://developer.nps.gov/api/v1/parks';
 
-
-function sendParkRequest(searchTerm, maxResults){
+function sendParkRequest(stateCode, limit){
   const options = {
     headers: new Headers({
       'X-Api-Key': apiKey})
   };
-  const url = 'https://developer.nps.gov/api/v1/parks';
-  fetch(url, options);
+
+  const params = {
+    stateCode,
+    limit
+  };
+  
+  const queryString = formatQueryParams(params);
+  const url = `${baseUrl}?${queryString}&api_key=50tWslfPEMTASf8Nddf6CgMuUIOAhvpt0WswKZIp`;
+  console.log(url);
+
+  fetch(url)
+    .then(data => data.json())
+    .then(response => console.log(response));
+}
+
+function formatQueryParams(paramsObject){
+  // return an interpolated string `?stateCode=${searchTerm}&limit=${maxResults}`
+  const paramsKeys = Object.keys(paramsObject)
+    .map(key => `${key}=${paramsObject[key]}`).join('&');
+  return paramsKeys;
 }
 
 function userSearchHandler() {
   $('.js-search').on('submit', event => {
     event.preventDefault();
 
-    const searchTerm = $('#state').val();
-    const maxResults = $('#max-results').val();
-    console.log(`searchTerm: ${searchTerm} and maxResults: ${maxResults}`);
-    sendParkRequest(searchTerm, maxResults);
+    const stateCode = $('#state').val();
+    const limit = $('#max-results').val();
+    sendParkRequest(stateCode, limit);
   });
 }
 
 function main(){
-
+  userSearchHandler();
 }
 
 $(main);
